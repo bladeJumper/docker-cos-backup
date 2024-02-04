3b9606771e4390e220f7b04212a018ecd9a091fc
Eric Xie <zhuojun.xie@hotmail.com>
2024-02-05 00:38:11 +0800

    * fix(entrypoint.sh): fix capitalization in webhook notification message * feat(entrypoint.sh): add support for notifying webhook with backup schedule information

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo "starting crond"
 crond -f
 if [ -n "$WEBHOOK_URL" ]; then
     echo "notifying ${WEBHOOK_URL}"
-    curl -m 10 --retry 5 $WEBHOOK_URL -F "title=Backup to ${BACKUP_NAME} Scheduled" -F "message=crontab: ${CRON_SCHEDULE}"
+    curl -m 10 --retry 5 $WEBHOOK_URL -F "title=Backup to ${BACKUP_NAME} scheduled" -F "message=crontab: ${CRON_SCHEDULE}"
 fi
