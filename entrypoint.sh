55877a70fe7b0ca11a1e57fd32a00f520f7af21b
Eric Xie <zhuojun.xie@hotmail.com>
2024-02-05 00:54:35 +0800

    * fix(entrypoint.sh): move crond command after the if statement * chore(entrypoint.sh): remove newline at end of file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 echo "creating crontab"
 echo -e "$CRON_SCHEDULE /backup.sh\n" > /etc/crontabs/root
 echo "starting crond"
-crond -f
 if [ -n "$WEBHOOK_URL" ]; then
     echo "notifying ${WEBHOOK_URL}"
     curl -m 10 --retry 5 $WEBHOOK_URL -F "title=Backup to ${BACKUP_NAME} scheduled" -F "message=crontab: ${CRON_SCHEDULE}"
 fi
+crond -f
