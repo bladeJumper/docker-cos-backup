070ad80dc28612c769d08ee470d1a6f665669ed5
Eric Xie <zhuojun.xie@hotmail.com>
2024-02-05 00:19:24 +0800

    enhance message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,6 @@ echo "creating crontab"
 echo -e "$CRON_SCHEDULE /backup.sh\n" > /etc/crontabs/root
 echo "starting crond"
 crond -f
+if [ -n "$WEBHOOK_URL" ]; then
+    curl -m 10 --retry 5 $WEBHOOK_URL -F "title=Backup to ${BACKUP_NAME} Scheduled" -F "message=crontab: ${CRON_SCHEDULE}"
+fi
