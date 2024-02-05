0e3a2d647e9d42b47a7a90b1c36c48832f2960da
Eric Xie <zhuojun.xie@hotmail.com>
2024-02-05 19:17:56 +0800

    * fix(entrypoint.sh): add conditional statements to check for BACKUP_FILE and BACKUP_PG variables before sending webhook notifications

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ echo -e "$CRON_SCHEDULE /backup.sh\n" > /etc/crontabs/root
 echo "starting crond"
 if [ -n "$WEBHOOK_URL" ]; then
     echo "notifying ${WEBHOOK_URL}"
-    curl -m 10 --retry 5 $WEBHOOK_URL -F "title=Backup to ${BACKUP_NAME} scheduled" -F "message=crontab: ${CRON_SCHEDULE}"
+    if [ -n "$BACKUP_FILE" ]; 
+    then
+        curl -m 10 --retry 5 $WEBHOOK_URL -F "title=Backup to ${BACKUP_NAME} scheduled" -F "message=crontab: ${CRON_SCHEDULE}"
+    fi
+    if [ -n "$BACKUP_PG" ]; 
+    then
+        curl -m 10 --retry 5 $WEBHOOK_URL -F "title=Database backup to ${PG_COS_FOLDER}${PG_COS_FILENAME} scheduled" -F "message=crontab: ${CRON_SCHEDULE}\ndatabase host: ${PG_HOST}"
+    fi
 fi
 crond -f
